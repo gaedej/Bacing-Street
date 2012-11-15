3206b9eb237e4370709aa4a8ff8497c5be52db8f
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-11-15 09:03:29 -0600

    Added Text Mining script

---
+++ TextMiningExperiment.R
@@ -0,0 +1,24 @@
+# init
+
+libs <- c("tm" , "plyr", "class")
+lapply(libs, require, character.only = TRUE)
+
+
+# clean text
+
+
+
+# build TDM
+
+# attach name
+
+
+
+# stack
+
+# hold-out
+
+
+# model - KNN
+
+# accuracy
