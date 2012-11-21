7b4c9d16f4b8fd98c0c1993d770ad6e65ada30b5
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-11-21 12:47:28 -0600

    Updataed

--- Create_Weeg_Query.R
+++ Create_Weeg_Query.R
@@ -1,5 +1,5 @@
 # This should create the faster construction of a query matrix
-Iterations <- 100
-Gender_1 <- rep.int(1,Iterations)
-Gender_1 <- rep(Gender_1, 4)
-cat(Gender_1)
+Iterations <- 1000000
+system.time(Gender_1 <- rep.int(1,Iterations))
+system.time(Gender_1 <- rep(Gender_1, 4))
+length(Gender_1)
