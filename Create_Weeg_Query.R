d4a68d97cd260a703526070194122615f894c839
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-11-21 15:21:48 -0600

    Added Math Component

--- Create_Weeg_Query.R
+++ Create_Weeg_Query.R
@@ -1,5 +1,28 @@
 # This should create the faster construction of a query matrix
-Iterations <- 1000000
+Iterations <- 10000
 system.time(Gender_1 <- rep.int(1,Iterations))
-system.time(Gender_1 <- rep(Gender_1, 4))
 length(Gender_1)
+# rm(Gender_1)
+
+
+system.time(Gender_2 <- rep.int(1,Iterations))
+length(Gender_2)
+
+system.time(Gender_3 <- rep.int(1,Iterations))
+length(Gender_3)
+
+system.time(Gender_4 <- rep.int(1,Iterations))
+length(Gender_4)
+
+TestMatrix <- matrix(data = NA, nrow = 4, ncol = length(Gender_1))
+TestMatrix[1,] <- Gender_1
+TestMatrix[2,] <- Gender_2
+TestMatrix[3,] <- Gender_3
+TestMatrix[4,] <- Gender_4
+
+
+# TestMatrix <- matrix(Gender_1, nrow = 1, ncol = length(Gender_1))
+# TestMatrix <- rbind(TestMatrix, Gender_2)
+# TestMatrix <- rbind(TestMatrix, Gender_3)
+# TestMatrix <- rbind(TestMatrix, Gender_4)
+
--- Weeg_Create_Vectors.R
+++ Weeg_Create_Vectors.R
@@ -9,31 +9,45 @@
 #          ,6,'Native Hawaiian/Other Pacific Islander' 
 #          ,7,'Two or more races' 
 #          ,0,'Prefer not to respond') ethnicity 
-Iterations <- 1000000
+Iterations <- 10000000
 
 Gender <- sample(0:2, Iterations, replace = TRUE)
 Grade <- sample(8:12, Iterations, replace = TRUE)
 Race <- sample(0:7, Iterations, replace = TRUE)
 District <- sample(0:50000, Iterations, replace = TRUE)
+ACTscoreComp <- sample(1:36, Iterations, replace = TRUE)
+ACTscoreEngl <- sample(1:36, Iterations, replace = TRUE)
+ACTscoreMath <- sample(1:36, Iterations, replace = TRUE)
+ACTscoreReadi <- sample(1:36, Iterations, replace = TRUE)
+ACTscoreScien <- sample(1:36, Iterations, replace = TRUE)
+
+BmatrixScores <-matrix(data = NA, nrow = 6, ncol = length(Gender))
+BmatrixScores[1,] <- Gender
+BmatrixScores[2,] <- ACTscoreComp
+BmatrixScores[3,] <- ACTscoreMath
+BmatrixScores[4,] <- ACTscoreReadi
+BmatrixScores[5,] <- ACTscoreScien
+
+Bmatrix3 <- matrix(data = NA, nrow = 4, ncol = length(Gender))
+Bmatrix3[1,] <- Gender
+Bmatrix3[2,] <- Grade
+Bmatrix3[3,] <- Race
+Bmatrix3[4,] <- District
+
+rm(Grade)
+rm(Race)
+rm(District)
 
 Gender_1 <- rep.int(1,Iterations)
 Grade_10 <- rep.int(10,Iterations)
 Race_3 <- rep.int(3, Iterations)
 District_1000 <- rep.int(1000, Iterations)
 
-Bmatrix3 <- matrix(Gender, nrow = 1, ncol = length(Gender))
-Bmatrix3 <- rbind(Bmatrix3, Grade)
-Bmatrix3 <- rbind(Bmatrix3, Race)
-Bmatrix3 <- rbind(Bmatrix3, District)
-
-rm(Grade)
-rm(Race)
-rm(District)
-
-Bmatrix4 <- matrix(Gender_1, nrow = 1, ncol = length(Gender_1))
-Bmatrix4 <- rbind(Bmatrix4, Grade_10)
-Bmatrix4 <- rbind(Bmatrix4, Race_3)
-Bmatrix4 <- rbind(Bmatrix4, District_1000)
+Bmatrix4 <- matrix(data = NA, nrow = 4, ncol = length(Gender_1))
+Bmatrix4[1,] <- Gender_1
+Bmatrix4[2,] <- Grade_10
+Bmatrix4[3,] <- Race_3
+Bmatrix4[4,] <- District_1000
 
 rm(Gender_1)
 rm(Grade_10)
@@ -53,4 +67,13 @@ length(Number_of_Trues)
 
 z[1:4,True_Col]
 True_Col
+
+cat("Comp Score")
+summary(BmatrixScores[2,True_Col])
+cat("Math Score")
+summary(BmatrixScores[3,True_Col])
+cat("Reading Score")
+summary(BmatrixScores[4,True_Col])
+cat("Science Score")
+summary(BmatrixScores[5,True_Col])
 
