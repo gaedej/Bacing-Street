052083be320b75a405b18e5f05c2e9ef854f19eb
Joel Gaede <joelgaede@Top-Secret-Do-Not-Hack.local>
2012-11-27 16:05:35 -0600

    Added and edited

--- Load_From_Mysql.R
+++ Load_From_Mysql.R
@@ -3,4 +3,6 @@ drv = dbDriver("MySQL")
 con = dbConnect(drv, user="gaedej", password="Fcgdaeb1", dbname="server_forcast",
                 host="inuinv001.corporate.act.org")
 dbListTables(con)
+dbListFields(con, "ossec_alerts")
 OA_Rule <- dbReadTable(con, "ossec_alerts", row.names = "oa_rule")
+summary(OA_Rule)
